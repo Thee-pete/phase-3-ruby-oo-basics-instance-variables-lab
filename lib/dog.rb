2f2f2e930a9194fd9b12adc2d8c17be3f53220e5
Peter Ndumia <ndegwandumia@gmail.com>
2022-08-22 02:25:27 +0300

    Completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,15 @@
 class Dog
 
-    # setter method
     def name=(dogs_name)
       @this_dogs_name = dogs_name
     end
-    
-    # getter method  
+  
     def name
       @this_dogs_name
     end
   end
-    
-  pete = Dog.new
-  pete.name = "Lassie"
-    
-  puts pete.name
+  
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  
+  puts lassie.name
